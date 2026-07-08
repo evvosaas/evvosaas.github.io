3c6eaa538ad795002c6c538cd064ee55299ae127
JhonyBeraldo <alquimia.indicador@gmail.com>
2026-07-08 12:13:38 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,4 +4,5 @@
 const EVVO_CONFIG = {
   SUPABASE_URL:  'https://fwlhibjkobkhckhpndmi.supabase.co',
   SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3bGhpYmprb2JraGNraHBuZG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNTg1NTQsImV4cCI6MjA5ODkzNDU1NH0.LIodREnE429jB3TfBSfK5Yjwzwgt6YNc2z_WgbIytyA',
+  PAGINA_FATURA: 'https://evvosaas.github.io/fatura.html',
 };
